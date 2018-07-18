16e243ec5533769dc3a934a67e378e77a811a1d6
steveguren <529543738@qq.com>
2018-07-18 15:14:46 +0800

    delete no use file

--- src/main/java/com/zzy/util/hash/HashRedisExecutor.java
+++
@@ -1,8 +0,0 @@
-package com.zzy.util.hash;
-
-import redis.clients.jedis.Jedis;
-
-// redis具体逻辑接口
-public interface HashRedisExecutor<T> {
-    T execute(Jedis jedis);
-}
--- src/main/java/com/zzy/util/sharded/ShardedRedisExecutor.java
+++
@@ -1,8 +0,0 @@
-package com.zzy.util.sharded;
-
-import redis.clients.jedis.ShardedJedis;
-
-// redis具体逻辑接口
-public interface ShardedRedisExecutor<T> {
-    T execute(ShardedJedis jedis);
-}
